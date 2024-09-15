6670ecedef4745e35cddd5078d4cb9ba59c0c426
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-09-15 18:43:24 +0200

    add eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+const prettier = require("eslint-config-prettier");
+const prettierPlugin = require("eslint-plugin-prettier");
+
+module.exports = [
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      prettier, // Disable conflicting ESLint rules with Prettier
+    ],
+    plugins: {
+      prettier: prettierPlugin, // Use Prettier as a plugin in the correct format
+    },
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+      "prettier/prettier": "error", // Prettier rule to enforce formatting
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  },
+];
--- src/app/projectlist/projectlist.component.ts
+++ src/app/projectlist/projectlist.component.ts
@@ -15,7 +15,7 @@ import { ProjectDetailsComponent } from '../project-details/project-details.comp
   template: `
     <div class="container text-center">
       <div class="row">
-        <div class="col">
+        <div *ngFor="let project of projects" class="col">
           <div class="card" style="width: 18rem;">
             <img src="..." class="card-img-top" alt="..." />
             <div class="card-body">
@@ -30,44 +30,12 @@ import { ProjectDetailsComponent } from '../project-details/project-details.comp
             </div>
           </div>
         </div>
-        <div class="col">
-          <div class="col">
-            <div class="card" style="width: 18rem;">
-              <img src="..." class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a [routerLink]="['/project-details']" class="btn btn-primary"
-                  >Go somewhere</a
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="col">
-            <div class="card" style="width: 18rem;">
-              <img src="..." class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a [routerLink]="['/project-details']" class="btn btn-primary"
-                  >Go somewhere</a
-                >
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   `,
   // templateUrl: './projectlist.component.html',
   styleUrl: './projectlist.component.scss',
 })
-export class ProjectlistComponent {}
+export class ProjectlistComponent {
+  projects: any = [1, 2, 3, 4];
+}
