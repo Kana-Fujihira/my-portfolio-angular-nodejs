29acc586e3a0ec81e5ac08018d558d79468284e4
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-10-27 13:27:10 +0100

    connected mongoDB

--- server/index.js
+++ server/index.js
@@ -1,21 +1,52 @@
-const express = require("express");
-const mongoose = require("mongoose");
+import express from "express";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
-mongoose
-  .connect(
-    "mongodb+srv://Kana:dRzxneXIG1DCj6yP@cluster0.exu00.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-  )
-  .then(() => console.log("DB conected!"))
-  .catch((err) => console.log(err));
+// __dirname を定義
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// 環境変数を読み込むための設定
+dotenv.config({ path: "./config/.env" });
 
 const app = express();
 
-app.get("/projects", function (req, res) {
+const uri = `mongodb+srv://Kana:${process.env.VITE_DB_KEY}@cluster0.exu00.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // クライアントをサーバーに接続する
+    await client.connect();
+    // 接続確認のためにpingを送信
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } catch (err) {
+    console.error("Failed to connect to MongoDB:", err);
+  } finally {
+    // クライアントの接続を閉じる
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", function (req, res) {
   res.json({ success: true });
 });
 
 const PORT = process.env.PORT || "3001";
 
 app.listen(PORT, function () {
-  console.log("I am runninig!");
+  console.log("I am running!");
 });
